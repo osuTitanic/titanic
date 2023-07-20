adbb18e9d38f21726b86853d835a8522e75b852a
Lekuru <levijordan007@gmail.com>
2023-07-20 21:39:41 +0200

    Rename #highlight to #announce

--- migrations/base.sql
+++ migrations/base.sql
@@ -282,6 +282,6 @@ VALUES (1, 0),
 
 INSERT INTO channels (name, topic, read_permissions, write_permissions)
 VALUES ('#osu', 'General discussion.', 1, 1),
-       ('#highlight', 'Public announcements.', 1, 8),
+       ('#announce', 'Public announcements.', 1, 8),
        ('#lobby', 'Multiplayer lobby discussion room.', 1, 1),
        ('#admin', 'General discussion for administrators.', 16, 16);
