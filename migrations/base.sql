ec4ac24b3cf387811bcfc804dd28d46c2ba24a66
Lekuru <contact@lekuru.xyz>
2024-03-19 11:19:32 +0100

    Refactor score indexes

--- migrations/base.sql
+++ migrations/base.sql
@@ -564,5 +564,6 @@ CREATE INDEX beatmaps_filename_idx ON beatmaps (filename);
 CREATE INDEX beatmaps_md5_idx ON beatmaps (md5);
 CREATE INDEX beatmaps_id_idx ON beatmaps (id);
 
-CREATE INDEX idx_beatmap_user_mode_status ON scores (beatmap_id, mode, user_id, status);
+CREATE INDEX idx_score_user_mode_status_pp ON scores (user_id, mode, status, pp DESC);
 CREATE INDEX idx_beatmap_mode_status ON scores (beatmap_id, mode, status);
+CREATE INDEX idx_beatmap_status ON scores (status);
