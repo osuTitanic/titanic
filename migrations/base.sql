df4cfbe046bbaa8bbc64a58754748229cdc8a6b6
Lekuru <contact@lekuru.xyz>
2024-07-01 17:48:49 +0200

    Add `clients_verified` table

--- migrations/base.sql
+++ migrations/base.sql
@@ -419,6 +419,19 @@ CREATE TABLE clients
     PRIMARY KEY (user_id, executable, adapters, unique_id, disk_signature)
 );
 
+-- Table for verified hardware ids, to
+-- bypass multiaccounting checks.
+-- Types:
+-- 0: Adapters
+-- 1: Unique Id
+-- 2: Disk Signature
+CREATE TABLE clients_verified
+(
+    "type" smallint NOT NULL,
+    "hash" character(32) NOT NULL,
+    PRIMARY KEY ("type", "hash")
+);
+
 CREATE TABLE logins
 (
     user_id int NOT NULL REFERENCES users (id),
@@ -585,6 +598,20 @@ VALUES (1, 1),    -- BanchoBot -> Admins
        (2, 1),    -- peppy -> Admins
        (2, 999),  -- peppy -> Supporter
        (2, 1000); -- peppy -> Players
+
+INSERT INTO clients_verified ("type", "hash")
+VALUES (0, 'b4ec3c4334a0249dae95c284ec5983df'), -- "runningunderwine"
+       (0, '74be16979710d4c4e7c6647856088456'), -- ""
+       (0, 'd41d8cd98f00b204e9800998ecf8427e'), -- ""
+       (1, 'ad921d60486366258809553a3db49a4a'), -- "unknown"
+       (1, '74be16979710d4c4e7c6647856088456'), -- ""
+       (1, 'd41d8cd98f00b204e9800998ecf8427e'), -- ""
+       (2, 'ad921d60486366258809553a3db49a4a'), -- "unknown"
+       (2, 'dcfcd07e645d245babe887e5e2daa016'), -- "0"
+       (2, '28c8edde3d61a0411511d3b1866f0636'), -- "1"
+       (2, '74be16979710d4c4e7c6647856088456'), -- ""
+       (2, 'd41d8cd98f00b204e9800998ecf8427e'), -- ""
+       (2, 'd1c651c36f499849f1c9a5843567e686'); -- toshiba hdd
 
 INSERT INTO user_count (count)
 VALUES (0);
