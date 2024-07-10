a56a35b78d690878a6cfb5ca6853ec2b4099f17a
Lekuru <contact@lekuru.xyz>
2024-07-10 21:02:19 +0200

    Add `migrations.sh` script

---
+++ scripts/migrations.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ -z "$DIABLE_CONFIG_OVERRIDE" ]; then
+  # Change your credentials here
+  POSTGRES_USER="bancho"
+  POSTGRES_PASSWORD="examplePassword"
+  POSTGRES_DB="bancho"
+  POSTGRES_PORT="5432"
+fi
+
+DATABASE_URL="postgres://$POSTGRES_USER:$POSTGRES_PASSWORD@127.0.0.1:$POSTGRES_PORT/$POSTGRES_DB?sslmode=disable"
+MIGRATIONS_DIR="../migrations"
+
+migrate -database "$DATABASE_URL" -path "$MIGRATIONS_DIR" $@
