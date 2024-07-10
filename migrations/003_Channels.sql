4ac43bea900e287c7960468223252e804ced49e5
Lekuru <contact@lekuru.xyz>
2024-07-10 17:03:07 +0200

    Split up clients, groups & channels

---
+++ migrations/003_Channels.sql
@@ -0,0 +1,5 @@
+INSERT INTO channels (name, topic, read_permissions, write_permissions)
+VALUES ('#osu', 'General discussion.', 1, 1),
+       ('#announce', 'Public announcements.', 1, 8),
+       ('#lobby', 'Multiplayer lobby discussion room.', 1, 1),
+       ('#admin', 'General discussion for administrators.', 16, 16);
---
+++ migrations/004_VerifiedClients.sql
@@ -0,0 +1,13 @@
+INSERT INTO clients_verified ("type", "hash")
+VALUES (0, 'b4ec3c4334a0249dae95c284ec5983df'), -- "runningunderwine"
+       (0, '74be16979710d4c4e7c6647856088456'), -- ""
+       (0, 'd41d8cd98f00b204e9800998ecf8427e'), -- ""
+       (1, 'ad921d60486366258809553a3db49a4a'), -- "unknown"
+       (1, '74be16979710d4c4e7c6647856088456'), -- ""
+       (1, 'd41d8cd98f00b204e9800998ecf8427e'), -- ""
+       (2, 'ad921d60486366258809553a3db49a4a'), -- "unknown"
+       (2, 'dcfcd07e645d245babe887e5e2daa016'), -- "0"
+       (2, '28c8edde3d61a0411511d3b1866f0636'), -- "1"
+       (2, '74be16979710d4c4e7c6647856088456'), -- ""
+       (2, 'd41d8cd98f00b204e9800998ecf8427e'), -- ""
+       (2, 'd1c651c36f499849f1c9a5843567e686'); -- toshiba hdd
