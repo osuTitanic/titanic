ac48f5ec88cb23098bd5bdea4790cc0b9eac7f9f
Lekuru <contact@lekuru.xyz>
2024-10-28 22:22:52 +0100

    Add beatmap packs table

---
+++ migrations/014_BeatmapPacks.down.sql
@@ -0,0 +1,5 @@
+DELETE FROM beatmap_pack_entries;
+DELETE FROM beatmap_packs;
+
+DROP TABLE beatmap_pack_entries;
+DROP TABLE beatmap_packs;
---
+++ migrations/014_BeatmapPacks.up.sql
@@ -0,0 +1,16 @@
+CREATE TABLE beatmap_packs (
+    id serial PRIMARY KEY,
+    name character varying(255) NOT NULL,
+    category character varying(255) NOT NULL,
+    description text NOT NULL DEFAULT '',
+    creator_id int NOT NULL REFERENCES users(id),
+    created_at timestamp without timezone NOT NULL DEFAULT now(),
+    updated_at timestamp without timezone NOT NULL DEFAULT now()
+);
+
+CREATE TABLE beatmap_pack_entries (
+    pack_id int NOT NULL REFERENCES beatmap_packs(id),
+    beatmap_id int NOT NULL REFERENCES beatmaps(id),
+    created_at timestamp without timezone NOT NULL DEFAULT now(),
+    PRIMARY KEY (pack_id, beatmap_id)
+);
