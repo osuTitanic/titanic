ab4dbec7caf29014d0bcd9ff8e595b375d9500b4
Lekuru <contact@lekuru.xyz>
2024-11-16 19:06:59 +0100

    Add pack indexes

--- migrations/014_BeatmapPacks.up.sql
+++ migrations/014_BeatmapPacks.up.sql
@@ -15,3 +15,9 @@ CREATE TABLE beatmap_pack_entries (
     created_at timestamp without time zone NOT NULL DEFAULT now(),
     PRIMARY KEY (pack_id, beatmapset_id)
 );
+
+CREATE INDEX beatmap_packs_creator_id ON beatmap_packs (creator_id);
+CREATE INDEX beatmap_packs_category ON beatmap_packs (category);
+
+CREATE INDEX beatmap_pack_entries_pack_id ON beatmap_pack_entries (pack_id);
+CREATE INDEX beatmap_pack_entries_beatmapset_id ON beatmap_pack_entries (beatmapset_id);
