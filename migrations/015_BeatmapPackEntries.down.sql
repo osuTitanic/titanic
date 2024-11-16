f98d327258ca97182adaf01834c6d390e4d2f34c
Lekuru <contact@lekuru.xyz>
2024-11-16 22:52:33 +0100

    Add pack entries data migration

---
+++ migrations/015_BeatmapPackEntries.down.sql
@@ -0,0 +1,2 @@
+DELETE FROM beatmap_pack_entries;
+DELETE FROM beatmap_packs;
---
+++ migrations/015_BeatmapPackEntries.up.sql
@@ -0,0 +1,254 @@
+INSERT INTO beatmap_packs (id, name, category, download_link, creator_id) VALUES (1, 'Video Game Pack vol.1', 'Standard', '', 1);
+INSERT INTO beatmap_pack_entries (pack_id, beatmapset_id) VALUES
+    (1, 1635),
+    (1, 1211),
+    (1, 1231),
+    (1, 1281),
+    (1, 1092),
+    (1, 312),
+    (1, 633),
+    (1, 688),
+    (1, 704),
+    (1, 154),
+    (1, 125),
+    (1, 92),
+    (1, 25);
+
+INSERT INTO beatmap_packs (id, name, category, download_link, creator_id) VALUES (2, 'Video Game Pack vol.2', 'Standard', '', 1);
+INSERT INTO beatmap_pack_entries (pack_id, beatmapset_id) VALUES
+    (2, 1044),
+    (2, 1123),
+    (2, 1367),
+    (2, 1525),
+    (2, 1818),
+    (2, 2008),
+    (2, 2128),
+    (2, 2147),
+    (2, 2404),
+    (2, 2420),
+    (2, 243),
+    (2, 2619),
+    (2, 628);
+
+INSERT INTO beatmap_packs (id, name, category, download_link, creator_id) VALUES (3, 'Video Game Pack vol.3', 'Standard', '', 1);
+INSERT INTO beatmap_pack_entries (pack_id, beatmapset_id) VALUES
+    (3, 1890),
+    (3, 2085),
+    (3, 2490),
+    (3, 2983),
+    (3, 3150),
+    (3, 3221),
+    (3, 3384),
+    (3, 3511),
+    (3, 3613),
+    (3, 4033),
+    (3, 4299),
+    (3, 4305),
+    (3, 4629);
+
+INSERT INTO beatmap_packs (id, name, category, download_link, creator_id) VALUES (4, 'Video Game Pack vol.4', 'Standard', '', 1);
+INSERT INTO beatmap_pack_entries (pack_id, beatmapset_id) VALUES
+    (4, 10104),
+    (4, 10880),
+    (4, 13489),
+    (4, 14205),
+    (4, 14458),
+    (4, 16669),
+    (4, 17373),
+    (4, 21836),
+    (4, 23073),
+    (4, 7077),
+    (4, 9580),
+    (4, 9668),
+    (4, 9854);
+
+INSERT INTO beatmap_packs (id, name, category, download_link, creator_id) VALUES (5, 'Anime Pack vol.1', 'Standard', '', 1);
+INSERT INTO beatmap_pack_entries (pack_id, beatmapset_id) VALUES
+    (5, 1005),
+    (5, 1377),
+    (5, 1414),
+    (5, 1464),
+    (5, 147),
+    (5, 1806),
+    (5, 301),
+    (5, 35),
+    (5, 442),
+    (5, 511),
+    (5, 584),
+    (5, 842),
+    (5, 897);
+
+INSERT INTO beatmap_packs (id, name, category, download_link, creator_id) VALUES (6, 'Anime Pack vol.2', 'Standard', '', 1);
+INSERT INTO beatmap_pack_entries (pack_id, beatmapset_id) VALUES
+    (6, 150),
+    (6, 162),
+    (6, 205),
+    (6, 212),
+    (6, 2207),
+    (6, 2267),
+    (6, 2329),
+    (6, 2425),
+    (6, 302),
+    (6, 496),
+    (6, 521),
+    (6, 86),
+    (6, 956);
+
+INSERT INTO beatmap_packs (id, name, category, download_link, creator_id) VALUES (7, 'Anime Pack vol.3', 'Standard', '', 1);
+INSERT INTO beatmap_pack_entries (pack_id, beatmapset_id) VALUES
+    (7, 2618),
+    (7, 3030),
+    (7, 4851),
+    (7, 4994),
+    (7, 5010),
+    (7, 5235),
+    (7, 5410),
+    (7, 5480),
+    (7, 5963),
+    (7, 6037),
+    (7, 6257),
+    (7, 6535),
+    (7, 6557);
+
+INSERT INTO beatmap_packs (id, name, category, download_link, creator_id) VALUES (8, 'Anime Pack vol.4', 'Standard', '', 1);
+INSERT INTO beatmap_pack_entries (pack_id, beatmapset_id) VALUES
+    (8, 12982),
+    (8, 13036),
+    (8, 13673),
+    (8, 14256),
+    (8, 14694),
+    (8, 16252),
+    (8, 21197),
+    (8, 516),
+    (8, 5438),
+    (8, 6301),
+    (8, 8422),
+    (8, 8829),
+    (8, 9556);
+
+INSERT INTO beatmap_packs (id, name, category, download_link, creator_id) VALUES (9, 'Internet! Pack vol.1', 'Standard', '', 1);
+INSERT INTO beatmap_pack_entries (pack_id, beatmapset_id) VALUES
+    (9, 66),
+    (9, 132),
+    (9, 140),
+    (9, 235),
+    (9, 303),
+    (9, 339),
+    (9, 455),
+    (9, 664),
+    (9, 812),
+    (9, 977),
+    (9, 1018),
+    (9, 1287);
+
+INSERT INTO beatmap_packs (id, name, category, download_link, creator_id) VALUES (10, 'Internet! Pack vol.2', 'Standard', '', 1);
+INSERT INTO beatmap_pack_entries (pack_id, beatmapset_id) VALUES
+    (10, 203),
+    (10, 917),
+    (10, 1573),
+    (10, 1628),
+    (10, 1785),
+    (10, 2103),
+    (10, 2569),
+    (10, 3196),
+    (10, 3219),
+    (10, 3545),
+    (10, 3621),
+    (10, 4535),
+    (10, 5014);
+
+INSERT INTO beatmap_packs (id, name, category, download_link, creator_id) VALUES (11, 'Internet! Pack vol.3', 'Standard', '', 1);
+INSERT INTO beatmap_pack_entries (pack_id, beatmapset_id) VALUES
+    (11, 1839),
+    (11, 3337),
+    (11, 3367),
+    (11, 3688),
+    (11, 5703),
+    (11, 5709),
+    (11, 5823),
+    (11, 6526),
+    (11, 6626),
+    (11, 7506),
+    (11, 7507),
+    (11, 8034),
+    (11, 8690);
+
+INSERT INTO beatmap_packs (id, name, category, download_link, creator_id) VALUES (12, 'Internet! Pack vol.4', 'Standard', '', 1);
+INSERT INTO beatmap_pack_entries (pack_id, beatmapset_id) VALUES
+    (12, 11443),
+    (12, 12033),
+    (12, 12155),
+    (12, 13885),
+    (12, 14391),
+    (12, 14579),
+    (12, 14672),
+    (12, 15157),
+    (12, 15628),
+    (12, 15942),
+    (12, 17145),
+    (12, 17217),
+    (12, 17724);
+
+INSERT INTO beatmap_packs (id, name, category, download_link, creator_id) VALUES (13, 'Rhythm Game Pack vol.1', 'Standard', '', 1);
+INSERT INTO beatmap_pack_entries (pack_id, beatmapset_id) VALUES
+    (13, 1452),
+    (13, 1450),
+    (13, 1078),
+    (13, 1201),
+    (13, 1300),
+    (13, 1317),
+    (13, 1338),
+    (13, 210),
+    (13, 296),
+    (13, 540),
+    (13, 564),
+    (13, 74),
+    (13, 96);
+
+INSERT INTO beatmap_packs (id, name, category, download_link, creator_id) VALUES (14, 'Rhythm Game Pack vol.2', 'Standard', '', 1);
+INSERT INTO beatmap_pack_entries (pack_id, beatmapset_id) VALUES
+    (14, 1207),
+    (14, 1567),
+    (14, 2534),
+    (14, 3302),
+    (14, 3435),
+    (14, 3499),
+    (14, 4887),
+    (14, 5087),
+    (14, 5177),
+    (14, 5275),
+    (14, 5321),
+    (14, 5349),
+    (14, 5577);
+
+INSERT INTO beatmap_packs (id, name, category, download_link, creator_id) VALUES (15, 'Rhythm Game Pack vol.3', 'Standard', '', 1);
+INSERT INTO beatmap_pack_entries (pack_id, beatmapset_id) VALUES
+    (15, 1206),
+    (15, 4357),
+    (15, 4617),
+    (15, 4772),
+    (15, 4954),
+    (15, 5180),
+    (15, 5672),
+    (15, 5696),
+    (15, 6598),
+    (15, 7094),
+    (15, 7237),
+    (15, 7612),
+    (15, 7983);
+
+INSERT INTO beatmap_packs (id, name, category, download_link, creator_id) VALUES (16, 'Rhythm Game Pack vol.4', 'Standard', '', 1);
+INSERT INTO beatmap_pack_entries (pack_id, beatmapset_id) VALUES
+    (16, 10842),
+    (16, 11135),
+    (16, 11488),
+    (16, 12052),
+    (16, 12190),
+    (16, 12710),
+    (16, 13249),
+    (16, 14572),
+    (16, 14778),
+    (16, 15241),
+    (16, 18492),
+    (16, 19809),
+    (16, 22401);
