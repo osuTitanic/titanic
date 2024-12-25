296378721ccc3d32c4d0d2c2f5fcd1d2d6dff19d
Lekuru <contact@lekuru.xyz>
2024-12-25 20:22:43 +0100

    Add score status indexes

--- migrations/019_ScoreStatus.down.sql
+++ migrations/019_ScoreStatus.down.sql
@@ -1,1 +1,5 @@
+DROP INDEX IF EXISTS idx_beatmap_status_score;
+DROP INDEX IF EXISTS idx_beatmap_mode_status_score;
+DROP INDEX IF EXISTS idx_score_user_mode_status_score_pp;
+
 ALTER TABLE scores DROP COLUMN "status_score";
--- migrations/019_ScoreStatus.up.sql
+++ migrations/019_ScoreStatus.up.sql
@@ -1,1 +1,5 @@
 ALTER TABLE scores ADD COLUMN "status_score" SMALLINT NOT NULL DEFAULT -1;
+
+CREATE INDEX IF NOT EXISTS idx_score_user_mode_status_score_pp ON scores (user_id, mode, status_score, pp DESC);
+CREATE INDEX IF NOT EXISTS idx_beatmap_mode_status_score ON scores (beatmap_id, mode, status_score);
+CREATE INDEX IF NOT EXISTS idx_beatmap_status_score ON scores (status_score);
