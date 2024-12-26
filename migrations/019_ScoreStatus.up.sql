5331039927d29cbfcf06c431c6f9c95871a1cc03
Lekuru <contact@lekuru.xyz>
2024-12-26 18:10:43 +0100

    Add better score status migration

--- migrations/019_ScoreStatus.up.sql
+++ migrations/019_ScoreStatus.up.sql
@@ -1,4 +1,5 @@
 ALTER TABLE scores ADD COLUMN "status_score" SMALLINT NOT NULL DEFAULT -1;
+UPDATE scores SET status_score = status;
 
 CREATE INDEX IF NOT EXISTS idx_score_user_mode_status_score_pp ON scores (user_id, mode, status_score, pp DESC);
 CREATE INDEX IF NOT EXISTS idx_beatmap_mode_status_score ON scores (beatmap_id, mode, status_score);
