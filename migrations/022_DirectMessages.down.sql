069cb5dfebac18470bee378e675c34e03132757b
Lekuru <contact@lekuru.xyz>
2025-01-24 19:23:05 +0100

    Revert dms migration on down

--- migrations/022_DirectMessages.down.sql
+++ migrations/022_DirectMessages.down.sql
@@ -1,2 +1,16 @@
+-- Revert migration
+INSERT INTO messages (sender, target, message, "time")
+SELECT
+    sender_user.name AS sender,
+    target_user.name AS target,
+    dm.message,
+    dm."time"
+FROM
+    direct_messages AS dm
+JOIN
+    users AS sender_user ON sender_user.id = dm.sender_id
+JOIN    
+    users AS target_user ON target_user.id = dm.target_id;
+
 DELETE FROM direct_messages;
 DROP TABLE direct_messages;
