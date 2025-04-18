0ab3dd83471fb0fd5dbc05e50501a03f7185ed06
Lekuru <contact@lekuru.xyz>
2025-04-18 20:26:11 +0200

    Add default permissions

--- migrations/026_Permissions.up.sql
+++ migrations/026_Permissions.up.sql
@@ -4,7 +4,7 @@ CREATE TABLE user_permissions (
     permission varchar(255) NOT NULL,
     rejected boolean DEFAULT false,
     created_at timestamp DEFAULT CURRENT_TIMESTAMP,
-    updated_at timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    updated_at timestamp DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE group_permissions (
@@ -13,10 +13,83 @@ CREATE TABLE group_permissions (
     permission varchar(255) NOT NULL,
     rejected boolean DEFAULT false,
     created_at timestamp DEFAULT CURRENT_TIMESTAMP,
-    updated_at timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    updated_at timestamp DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE INDEX idx_user_permissions_user_id ON user_permissions(user_id);
 CREATE INDEX idx_group_permissions_group_id ON group_permissions(group_id);
 CREATE INDEX idx_user_permissions_user_id_rejected ON user_permissions(user_id, rejected);
 CREATE INDEX idx_group_permissions_group_id_rejected ON group_permissions(group_id, rejected);
+
+INSERT INTO group_permissions (group_id, permission, rejected) VALUES
+    -- Admins
+    (1, '*', false),
+    -- Developers
+    (2, '*', false),
+    -- BAT
+    (3, 'beatmaps.*', false),
+    (3, 'forum.topics.lock', false),
+    (3, 'forum.posts.lock', false),
+    (3, 'forum.posts.edit_locked', false),
+    (3, 'forum.beatmaps.edit_icon', false),
+    (3, 'forum.kudosu.*', false),
+    -- Moderators
+    (4, 'beatmaps.nuke', false),
+    (4, 'users.moderation.*', false),
+    (4, 'forum.moderation.*', false),
+    (3, 'chat.moderation.*', false),
+    -- Tournament Manager Team
+    (5, 'commands.multiplayer.tournaments.create_match', false),
+    (5, 'commands.multiplayer.tournaments.force_invite', false),
+    (5, 'commands.multiplayer.tournaments.addref', false),
+    (5, 'commands.multiplayer.tournaments.remref', false),
+    -- Donator
+    (6, 'clients.preview', false),
+    -- Bots
+    (9, 'chat.bypass_filter', false),
+    (9, 'chat.bypass_spam', false),
+    -- TMG
+    (10, 'clients.*', false),
+    -- Preview
+    (997, 'clients.validation.bypass', false),
+    -- Bypass
+    (998, 'scoring.validation.bypass', false),
+    -- "Supporter"
+    (999, 'beatmaps.direct.search', false),
+    (999, 'beatmaps.direct.download', false),
+    (999, 'beatmaps.leaderboards.country', false),
+    (999, 'beatmaps.leaderboards.friends', false),
+    (999, 'beatmaps.leaderboards.mods', false),
+    (999, 'beatmaps.favourites.extended_limit', false),
+    (999, 'beatmaps.upload.extended_limit', false),
+    (999, 'users.friends.extended_limit', false),
+    -- Regular
+    (1000, 'users.notifications.delete', false),
+    (1000, 'users.profile.update', false),
+    (1000, 'users.friends.delete', false),
+    (1000, 'users.friends.add', false),
+    (1000, 'beatmaps.search', false),
+    (1000, 'beatmaps.upload', false),
+    (1000, 'beatmaps.revive', false),
+    (1000, 'beatmaps.download', false),
+    (1000, 'commands.standard.*', false),
+    (1000, 'commands.multiplayer.standard.*', false),
+    (1000, 'benchmarks.view', false),
+    (1000, 'benchmarks.submit', false),
+    (1000, 'chat.search', false),
+    (1000, 'chat.messages.view', false),
+    (1000, 'chat.messages.create', false),
+    (1000, 'chat.messages.private.view', false),
+    (1000, 'chat.messages.private.create', false),
+    (1000, 'forum.search', false),
+    (1000, 'forum.topics.create', false),
+    (1000, 'forum.posts.create', false),
+    (1000, 'forum.posts.delete', false),
+    (1000, 'forum.bookmarks.create', false),
+    (1000, 'forum.bookmarks.delete', false),
+    (1000, 'forum.subscriptions.create', false),
+    (1000, 'forum.subscriptions.delete', false),
+    (1000, 'bancho.matches.create', false),
+    (1000, 'bancho.matches.join', false),
+    (1000, 'bancho.spectating.start', false),
+    (1000, 'bancho.spectating.send_frames', false);
