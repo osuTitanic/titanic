3415188af15c668515e5912178f50c4305379e52
Lekuru <contact@lekuru.xyz>
2025-08-29 19:00:59 +0200

    Add indexes to history `created_at` columns

--- migrations/041_HistoryDates.down.sql
+++ migrations/041_HistoryDates.down.sql
@@ -1,3 +1,6 @@
+DROP INDEX IF EXISTS idx_replay_history_user_mode_created_at;
+DROP INDEX IF EXISTS idx_play_history_user_mode_created_at;
+
 ALTER TABLE profile_play_history
 DROP COLUMN created_at;
 
--- migrations/041_HistoryDates.up.sql
+++ migrations/041_HistoryDates.up.sql
@@ -15,3 +15,9 @@ SET created_at = make_date(year, month, 1);
 
 ALTER TABLE profile_replay_history
 ALTER COLUMN created_at SET NOT NULL;
+
+CREATE INDEX idx_replay_history_user_mode_created_at
+ON profile_replay_history (user_id, mode, created_at DESC);
+
+CREATE INDEX idx_play_history_user_mode_created_at
+ON profile_play_history (user_id, mode, created_at DESC);
