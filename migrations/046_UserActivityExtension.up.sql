41c2a65f984c6bda6c71d3fcf21ac5ada46b0980
Lekuru <contact@lekuru.xyz>
2025-12-06 14:01:46 +0100

    Fix `count` value in `user_count` compatibility view

--- migrations/046_UserActivityExtension.up.sql
+++ migrations/046_UserActivityExtension.up.sql
@@ -4,4 +4,4 @@ ALTER TABLE user_activity RENAME COLUMN count TO osu_count;
 ALTER TABLE user_activity ADD COLUMN irc_count INTEGER DEFAULT 0;
 ALTER TABLE user_activity ADD COLUMN mp_count INTEGER DEFAULT 0;
 
-CREATE VIEW user_count AS SELECT time, osu_count AS count FROM user_activity;
+CREATE VIEW user_count AS SELECT time, (osu_count + irc_count) AS count FROM user_activity;
