2651e2081b786f02c2a0ea1ac4196e4287ea9f10
Lekuru <contact@lekuru.xyz>
2026-06-12 21:46:25 +0200

    Add password reset email format

--- internal/constants/emails.go
+++ internal/constants/emails.go
@@ -16,3 +16,17 @@ See you in game!
 --
 Titanic! | %s
 `
+
+const EmailPasswordResetSubject = "Reset your password"
+const EmailPasswordResetBody = `Hi %s,
+
+You are receiving this notification because you have (or someone pretending to be you has) requested a new password be sent for your account on.
+If you did not request this notification, then please ignore it. If you keep receiving it, please contact an administrator.
+
+To use the new password, you need to activate it by clicking the link provided below.
+
+%s/account/verification?id=%d&token=%s&type=password
+
+--
+Titanic! | %s
+`
