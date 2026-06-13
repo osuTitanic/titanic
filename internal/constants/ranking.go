8fda12dba8452d9c1866a011d4cda039807f24bd
Lekuru <contact@lekuru.xyz>
2026-06-13 13:51:39 +0200

    Add `NewRankingTypeFromAlias` helper

--- internal/constants/ranking.go
+++ internal/constants/ranking.go
@@ -34,3 +34,26 @@ func (rankingType RankingType) String() string {
 		return fmt.Sprintf("Unknown (%s)", string(rankingType))
 	}
 }
+
+func NewRankingTypeFromAlias(alias string) (RankingType, bool) {
+	switch alias {
+	case "global":
+		return RankingTypePerformance, true
+	case "performance":
+		return RankingTypePerformance, true
+	case "country":
+		return RankingTypeCountry, true
+	case "tscore":
+		return RankingTypeTotalScore, true
+	case "rscore":
+		return RankingTypeRankedScore, true
+	case "ppv1":
+		return RankingTypePPv1, true
+	case "clears":
+		return RankingTypeClears, true
+	case "leader":
+		return RankingTypeLeader, true
+	default:
+		return "", false
+	}
+}
