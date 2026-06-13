17874926882156e28e96681111588d956f26efe2
Lekuru <contact@lekuru.xyz>
2026-06-13 12:58:04 +0200

    Refactor ranking type constants

--- internal/constants/ranking.go
+++ internal/constants/ranking.go
@@ -1,12 +1,36 @@
 package constants
 
+import "fmt"
+
 type RankingType string
 
 const (
-	RankingTypeGlobal      RankingType = "global"
+	RankingTypePerformance RankingType = "performance"
 	RankingTypeCountry     RankingType = "country"
 	RankingTypeTotalScore  RankingType = "tscore"
 	RankingTypeRankedScore RankingType = "rscore"
 	RankingTypePPv1        RankingType = "ppv1"
 	RankingTypeClears      RankingType = "clears"
+	RankingTypeLeader      RankingType = "leader"
 )
+
+func (rankingType RankingType) String() string {
+	switch rankingType {
+	case RankingTypePerformance:
+		return "Performance"
+	case RankingTypeCountry:
+		return "Country"
+	case RankingTypeTotalScore:
+		return "Total Score"
+	case RankingTypeRankedScore:
+		return "Ranked Score"
+	case RankingTypePPv1:
+		return "PPv1"
+	case RankingTypeClears:
+		return "Clears"
+	case RankingTypeLeader:
+		return "First Places"
+	default:
+		return fmt.Sprintf("Unknown (%s)", string(rankingType))
+	}
+}
