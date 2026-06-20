f9cfe22f84fab0c8dfb21bfbe014d9bf8043b30b
Lekuru <contact@lekuru.xyz>
2026-06-20 19:15:33 +0200

    Add group id constants

---
+++ internal/constants/groups.go
@@ -0,0 +1,18 @@
+package constants
+
+const (
+	GroupAdmin     = 1
+	GroupDeveloper = 2
+	GroupBAT       = 3
+	GroupGMT       = 4
+	GroupTMT       = 5
+	GroupDonator   = 6
+	GroupAlumni    = 7
+	GroupEarly     = 8
+	GroupBot       = 9
+	GroupModding   = 10
+	GroupPreview   = 997
+	GroupBypass    = 998
+	GroupSupporter = 999
+	GroupPlayers   = 1000
+)
