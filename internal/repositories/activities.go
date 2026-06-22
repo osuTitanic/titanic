de4e19079e9445b1ac6f48640bcd3aba5d4dfa63
Lekuru <contact@lekuru.xyz>
2026-06-22 18:31:45 +0200

    Add `Activities.FetchRecentByUser` query

--- internal/repositories/activities.go
+++ internal/repositories/activities.go
@@ -1,6 +1,8 @@
 package repositories
 
 import (
+	"time"
+
 	"github.com/osuTitanic/titanic-go/internal/schemas"
 	"gorm.io/gorm"
 )
@@ -23,4 +25,18 @@ func (r *ActivityRepository) Delete(activity *schemas.Activity) error {
 
 func (r *ActivityRepository) Update(updates *schemas.Activity, columns ...string) (int64, error) {
 	return CommonUpdate(r.db, updates, columns...)
+}
+
+func (r *ActivityRepository) FetchRecentByUser(userId int, mode int, limit int, offset int, until time.Duration, preload ...string) ([]*schemas.Activity, error) {
+	var activities []*schemas.Activity
+	err := Preloaded(r.db, preload).
+		Where("user_id = ?", userId).
+		Where("mode = ? OR mode IS NULL", mode).
+		Where("time > ?", time.Now().Add(-until)).
+		Where("hidden = ?", false).
+		Order("id DESC").
+		Offset(offset).
+		Limit(limit).
+		Find(&activities).Error
+	return activities, err
 }
