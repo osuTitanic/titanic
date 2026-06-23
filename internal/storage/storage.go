66388e1048ae22e51cfaebc44bbff938da23dcf9
Lekuru <contact@lekuru.xyz>
2026-06-23 19:34:01 +0200

    `bucket` -> `directory`

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -5,12 +5,12 @@ import "io"
 // Storage defines the interface for a storage backend
 type Storage interface {
 	Setup() error
-	Save(key string, bucket string, data []byte) error
-	SaveStream(key string, bucket string, stream io.Reader) error
-	Read(key string, bucket string) ([]byte, error)
-	ReadStream(key string, bucket string) (io.ReadSeekCloser, error)
-	Remove(key string, bucket string) error
-	Exists(key string, bucket string) bool
+	Save(key string, directory string, data []byte) error
+	SaveStream(key string, directory string, stream io.Reader) error
+	Read(key string, directory string) ([]byte, error)
+	ReadStream(key string, directory string) (io.ReadSeekCloser, error)
+	Remove(key string, directory string) error
+	Exists(key string, directory string) bool
 }
 
 var RequiredDirectories = []string{
