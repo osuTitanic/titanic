47c9b6a912cf1023ebcb628256d5933d2025fd54
Lekuru <contact@lekuru.xyz>
2026-07-12 13:42:08 +0200

    Add simple in-memory cache for testing

--- internal/performance/ppv2_caching.go
+++ internal/performance/ppv2_caching.go
@@ -1,6 +1,31 @@
 package performance
 
+import (
+	"time"
+
+	"github.com/osuTitanic/titanic/internal/caching"
+)
+
+const ppv2CacheExpiry = time.Hour * 24
+
 type PPv2CacheLayer interface {
 	ToCache(beatmapId int, data any) bool
-	FromCache(beatmapId int) (bool, any)
+	FromCache(beatmapId int) (any, bool)
+}
+
+type PPv2MemoryCache struct {
+	cache *caching.Cache[int, any]
+}
+
+func NewPPv2MemoryCache() *PPv2MemoryCache {
+	return &PPv2MemoryCache{cache: caching.New[int, any](ppv2CacheExpiry)}
+}
+
+func (c *PPv2MemoryCache) ToCache(beatmapId int, data any) bool {
+	c.cache.Set(beatmapId, data)
+	return true // in-memory cache always succeeds
+}
+
+func (c *PPv2MemoryCache) FromCache(beatmapId int) (any, bool) {
+	return c.cache.Get(beatmapId)
 }
