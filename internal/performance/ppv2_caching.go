a6931ea4bac26a8d3f2b509553925eb2ca52e461
Lekuru <contact@lekuru.xyz>
2026-07-12 14:54:21 +0200

    Add ppv2 redis cache layer implementation

--- internal/performance/ppv2_caching.go
+++ internal/performance/ppv2_caching.go
@@ -1,13 +1,23 @@
 package performance
 
 import (
+	"bytes"
+	"context"
+	"encoding/gob"
+	"fmt"
 	"time"
 
 	"github.com/osuTitanic/titanic/internal/caching"
 	"github.com/osuTitanic/titanic/internal/constants"
+	"github.com/redis/go-redis/v9"
 )
 
 const ppv2CacheExpiry = time.Hour * 24
+
+type PPv2CacheLayer interface {
+	ToCache(key PPv2CacheKey, data any) bool
+	FromCache(key PPv2CacheKey) (any, bool)
+}
 
 type PPv2CacheKey struct {
 	BeatmapId int
@@ -15,9 +25,8 @@ type PPv2CacheKey struct {
 	Mods      constants.Mods
 }
 
-type PPv2CacheLayer interface {
-	ToCache(key PPv2CacheKey, data any) bool
-	FromCache(key PPv2CacheKey) (any, bool)
+func (key *PPv2CacheKey) String() string {
+	return fmt.Sprintf("%d-%d-%d", key.BeatmapId, key.Mode, key.Mods)
 }
 
 type PPv2MemoryCache struct {
@@ -35,4 +44,34 @@ func (c *PPv2MemoryCache) ToCache(key PPv2CacheKey, data any) bool {
 
 func (c *PPv2MemoryCache) FromCache(key PPv2CacheKey) (any, bool) {
 	return c.cache.Get(key)
+}
+
+type PPv2RedisCache struct {
+	client *redis.Client
+}
+
+func NewPPv2RedisCache(client *redis.Client) *PPv2RedisCache {
+	return &PPv2RedisCache{client: client}
+}
+
+func (c *PPv2RedisCache) ToCache(key PPv2CacheKey, data any) bool {
+	buffer := bytes.NewBuffer([]byte{})
+	encoder := gob.NewEncoder(buffer)
+	if err := encoder.Encode(data); err != nil {
+		return false
+	}
+
+	status := c.client.Set(context.Background(), key.String(), buffer.Bytes(), ppv2CacheExpiry)
+	return status.Err() != nil
+}
+
+func (c *PPv2RedisCache) FromCache(key PPv2CacheKey) (result any, ok bool) {
+	data, err := c.client.Get(context.Background(), key.String()).Bytes()
+	if err != nil {
+		return nil, false
+	}
+
+	reader := bytes.NewReader(data)
+	err = gob.NewDecoder(reader).Decode(result)
+	return result, err != nil
 }
