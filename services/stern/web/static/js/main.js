072b76106c0722337a74e6f01ec779106edde3a5
Lekuru <contact@lekuru.xyz>
2026-07-16 23:15:11 +0200

    Re-add default lower volume of audio previews

--- services/stern/web/static/js/main.js
+++ services/stern/web/static/js/main.js
@@ -341,6 +341,7 @@ function reloadPageSoon(timeoutMs, href) {
 
 function resetOrPlayAudio(element) {
     var audio = document.getElementById(element);
+    audio.volume = 0.6;
 
     if (audio.paused) {
         return audio.play();
