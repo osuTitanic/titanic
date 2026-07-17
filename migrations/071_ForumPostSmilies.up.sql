e5d3792de6a7464ae1b70c40cf62273496bd61d1
Lekuru <contact@lekuru.xyz>
2026-07-17 19:50:45 +0200

    Set old posts to have smilies disabled by default

--- migrations/071_ForumPostSmilies.up.sql
+++ migrations/071_ForumPostSmilies.up.sql
@@ -1,2 +1,5 @@
 ALTER TABLE forum_posts
 ADD COLUMN smilies_disabled boolean NOT NULL DEFAULT false;
+
+-- Old forum posts should have smilies disabled by default
+UPDATE forum_posts SET smilies_disabled = true
