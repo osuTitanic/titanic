29c231ded7ea773fcef7d01b7f322957579890b8
Lekuru <contact@lekuru.xyz>
2026-07-20 18:57:35 +0200

    Implement "foes" to friends page -> rename to "Friends & Foes"
    also made use of some more jquery

--- services/stern/web/static/js/settings.js
+++ services/stern/web/static/js/settings.js
@@ -1,13 +1,14 @@
 function handleNotificationClick(event, linkElement) {
     event.preventDefault();
 
+    var linkElement = $(linkElement);
     var notificationElement = linkElement.closest(".notification");
-    var notificationId = notificationElement.id;
-    var targetUrl = linkElement.href;
+    var notificationId = notificationElement.attr("id");
+    var targetUrl = linkElement.attr("href");
 
     // Mark notification as read
     performApiRequest("DELETE", "/account/notifications/" + notificationId, null, function (xhr) {
-        notificationElement.classList.remove("new");
+        notificationElement.removeClass("new");
 
         // Redirect to the target URL after marking as read
         if (targetUrl) window.location.href = targetUrl;
@@ -19,12 +20,12 @@ function handleNotificationClick(event, linkElement) {
 function handleNotificationBoxClick(event, notificationId) {
     event.preventDefault();
 
-    var notificationElement = document.getElementById(notificationId);
-    var targetUrl = notificationElement.querySelector(".notification-link").href;
+    var notificationElement = $("#" + notificationId);
+    var targetUrl = notificationElement.find(".notification-link").attr("href");
 
     // Mark notification as read
     performApiRequest("DELETE", "/account/notifications/" + notificationId, null, function (xhr) {
-        notificationElement.classList.remove("new");
+        notificationElement.removeClass("new");
 
         // Redirect to the target URL after marking as read
         if (targetUrl) window.location.href = targetUrl;
@@ -35,63 +36,165 @@ function handleNotificationBoxClick(event, notificationId) {
 
 function clearAllNotifications() {
     performApiRequest("DELETE", "/account/notifications", null, function (xhr) {
-        var elements = document.querySelectorAll(".notification");
-        for (var i = 0; i < elements.length; i++) {
-            elements[i].classList.remove("new");
-            elements[i].onclick = function () {};
-        }
-
+        $(".notification").removeClass("new").removeAttr("onclick");
         reloadPageSoon(500);
     });
 }
 
 function removeBookmark(topicId) {
     performApiRequest("DELETE", "/forum/bookmarks/" + topicId, null, function (xhr) {
-        var data = JSON.parse(xhr.responseText);
-        var bookmark = document.getElementById("bookmark-" + topicId);
-        bookmark.parentNode.removeChild(bookmark);
-
-        var totalBookmarks = document.querySelectorAll(".bookmark").length;
-        if (totalBookmarks === 0) {
-            var bookmarks = document.querySelector(".bookmarks");
-            bookmarks.innerHTML = "You have no bookmarks";
+        $("#bookmark-" + topicId).remove();
+        if ($(".bookmark").length === 0) {
+            $(".bookmarks").text("You have no bookmarks");
         }
     });
     return false;
 }
 
-function removeFriend(element) {
-    if (!isLoggedIn()) return;
+function lookupUser(username, callbackSuccess, callbackError) {
+    if (!username) {
+        if (callbackError) callbackError(null);
+        return;
+    }
 
-    performApiRequest("DELETE", "/account/friends?id=" + element.id, null, function (xhr) {
-        var data = JSON.parse(xhr.responseText);
-        var friendContainer = element.parentElement.parentElement;
-        friendContainer.style.opacity = 0;
+    performApiRequest(
+        "GET",
+        "/users/lookup/" + encodeURIComponent(username),
+        null,
+        function (xhr) {
+            var user = $.parseJSON(xhr.responseText);
+            if (callbackSuccess) callbackSuccess(user);
+        },
+        function (xhr) {
+            if (callbackError) callbackError(xhr);
+        }
+    );
+}
 
-        setTimeout(function () {
-            friendContainer.parentNode.removeChild(friendContainer);
-        }, 350);
-    });
+function lookupUserFromSettings(form, selfMessage, callback) {
+    if (!isLoggedIn()) return false;
+
+    var formElement = $(form);
+    var username = $.trim(formElement.find("[name=username]").val());
+    var button = formElement.find("button").first();
+
+    if (!username) {
+        alert("Please enter a username.");
+        return false;
+    }
+
+    button.prop("disabled", true);
+    lookupUser(
+        username,
+        function (user) {
+            if (user.id === currentUser) {
+                button.prop("disabled", false);
+                alert(selfMessage);
+                return;
+            }
+
+            callback(user, button);
+        },
+        function (xhr) {
+            button.prop("disabled", false);
+            apiErrorAlert(xhr, "The user could not be found.");
+        }
+    );
     return false;
 }
 
+function addFriendFromSettings(form) {
+    return lookupUserFromSettings(
+        form,
+        "You cannot add yourself as a friend.",
+        function (user, button) {
+            addFriend(
+                user.id,
+                function () {
+                    reloadPageSoon(250);
+                },
+                function (xhr) {
+                    button.prop("disabled", false);
+                    apiErrorAlert(xhr, "The user could not be added as a friend.");
+                }
+            );
+        }
+    );
+}
+
+function removeFriendFromSettings(element) {
+    return removeRelationshipFromSettings(
+        element,
+        removeFriend,
+        "The user could not be removed from your friends."
+    );
+}
+
+function addFoeFromSettings(form) {
+    return lookupUserFromSettings(
+        form,
+        "You cannot block yourself.",
+        function (user, button) {
+            if (!window.confirm("Block " + user.name + "?")) {
+                button.prop("disabled", false);
+                return;
+            }
+
+            addFoe(
+                user.id,
+                function () {
+                    reloadPageSoon(250);
+                },
+                function (blockXhr) {
+                    button.prop("disabled", false);
+                    apiErrorAlert(blockXhr, "The user could not be blocked.");
+                }
+            );
+        }
+    );
+}
+
+function removeFoeFromSettings(element) {
+    return removeRelationshipFromSettings(
+        element,
+        removeFoe,
+        "The user could not be unblocked."
+    );
+}
+
+function removeRelationshipFromSettings(element, removeRelationship, errorMessage) {
+    if (!isLoggedIn()) return false;
+
+    var relationshipElement = $(element);
+    return removeRelationship(
+        relationshipElement.attr("id"),
+        function () {
+            var relationshipCard = relationshipElement.closest(".relationship-card");
+            relationshipCard.fadeOut(350, function () {
+                relationshipCard.remove();
+            });
+        },
+        function (xhr) {
+            apiErrorAlert(xhr, errorMessage);
+        }
+    );
+}
+
 function dataExport() {
-    var passwordValidation = document.getElementById("data-export-password");
-    var password = passwordValidation.value;
+    var password = $("#data-export-password").val();
     if (!password) {
         alert("Please enter your current password to confirm the data export.");
         return;
     }
     var data = { password: password };
 
-    var recaptchaResponse = document.getElementById("recaptcha-response");
-    if (recaptchaResponse && recaptchaResponse.value) {
-        data["recaptcha_response"] = recaptchaResponse.value;
+    var recaptchaResponse = $("#recaptcha-response").val();
+    if (recaptchaResponse) {
+        data["recaptcha_response"] = recaptchaResponse;
     }
 
-    var exportButton = document.getElementById("data-export-button");
-    exportButton.disabled = true;
-    exportButton.value = "Exporting...";
+    var exportButton = $("#data-export-button");
+    exportButton.prop("disabled", true).val("Exporting...");
 
     performApiRequest(
         "POST",
@@ -102,23 +205,15 @@ function dataExport() {
             var url = URL.createObjectURL(blob);
 
             // Invoke download prompt with json response data
-            var a = document.createElement("a");
-            a.download = "data_export.json";
-            a.href = url;
-            a.click();
-
-            exportButton.value = "Exported!";
+            var downloadLink = $("<a>")
+                .attr("download", "data_export.json")
+                .attr("href", url);
+            downloadLink[0].click();
+            exportButton.val("Exported!");
         },
         function (xhr) {
-            var response = JSON.parse(xhr.responseText);
-            if (!response.details) {
-                alert("An error occurred while exporting your data. Please try again later.");
-            } else {
-                alert(response.details);
-            }
-
-            exportButton.disabled = false;
-            exportButton.value = "Export";
+            apiErrorAlert(xhr, "An error occurred while exporting your data. Please try again later.");
+            exportButton.prop("disabled", false).val("Export");
         }
     );
 }
