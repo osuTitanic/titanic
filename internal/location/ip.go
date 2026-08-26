69378528549cb9ca4d1d757706e44d1e4166db71
Levi <contact@lekuru.xyz>
2026-08-26 20:42:26 +0200

    Improve IP address parsing in `IsLocalIP`

--- internal/location/ip.go
+++ internal/location/ip.go
@@ -1,13 +1,14 @@
 package location
 
-import "net"
+import "net/netip"
 
 // IsLocalIP reports whether the given IP address is a local / non-routable address.
 func IsLocalIP(ip string) bool {
-	address := net.ParseIP(ip)
-	if address == nil {
+	address, err := netip.ParseAddr(ip)
+	if err != nil {
 		return false
 	}
+	address = address.Unmap()
 
 	return address.IsPrivate() ||
 		address.IsLoopback() ||
