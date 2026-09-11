043003bf73559f052428caa5d068031f29e6453a
Lekuru <contact@lekuru.xyz>
2026-09-11 20:59:53 +0200

    Wrap `Process` actions in database transaction

--- services/deck/internal/scoring/processor.go
+++ services/deck/internal/scoring/processor.go
@@ -4,46 +4,58 @@ import (
 	"fmt"
 
 	"github.com/osuTitanic/titanic/internal/state"
+	"github.com/osuTitanic/titanic/services/deck/internal/server"
 )
 
 type Processor struct {
-	app        *state.State
-	submission *SubmissionContext
+	Context      *server.Context
+	Submission   *SubmissionContext
+	Repositories *state.Repositories
 }
 
-func NewProcessor(app *state.State, submission *SubmissionContext) *Processor {
-	return &Processor{app: app, submission: submission}
+func NewProcessor(ctx *server.Context, submission *SubmissionContext) *Processor {
+	return &Processor{
+		Context:    ctx,
+		Submission: submission,
+	}
 }
 
 func (processor *Processor) Process(password string) (result Result, err error) {
-	result.Type = ResultAccepted
-	result.Submission = processor.submission
-
-	result.Type, err = processor.Prepare(password)
-	if err != nil {
-		return result, fmt.Errorf("prepare score submission: %w", err)
-	}
-	if result.Rejected() {
-		return result, nil
+	result = Result{
+		Type:       ResultAccepted,
+		Submission: processor.Submission,
+		Warnings:   []error{},
 	}
 
-	result.Type, err = processor.Validate()
-	if err != nil {
-		return result, fmt.Errorf("validate score submission: %w", err)
-	}
-	if result.Rejected() {
-		return result, nil
-	}
+	err = processor.Context.State.DatabaseTransaction(func(repos *state.Repositories) error {
+		processor.Repositories = repos
 
-	// TODO: We should pass in a db transaction here
-	result.Type, err = processor.Persist()
-	if err != nil {
-		return result, fmt.Errorf("persist score submission: %w", err)
-	}
-	if result.Rejected() {
-		return result, nil
-	}
+		result.Type, err = processor.Prepare(password)
+		if err != nil {
+			return fmt.Errorf("prepare score submission: %w", err)
+		}
+		if result.Rejected() {
+			return nil
+		}
 
-	result.Warnings = processor.PostProcess()
-	return result, nil
+		result.Type, err = processor.Validate()
+		if err != nil {
+			return fmt.Errorf("validate score submission: %w", err)
+		}
+		if result.Rejected() {
+			return nil
+		}
+
+		result.Type, err = processor.Persist()
+		if err != nil {
+			return fmt.Errorf("persist score submission: %w", err)
+		}
+		if result.Rejected() {
+			return nil
+		}
+
+		result.Warnings = processor.PostProcess()
+		return nil
+	})
+	return result, err
 }
