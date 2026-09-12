5ed3f43b0678ed11161314632546e8f93adbc4d1
Lekuru <contact@lekuru.xyz>
2026-09-12 13:47:14 +0200

    Add basic ppv1 & ppv2 calculators to validate

--- services/deck/internal/scoring/processor_validate.go
+++ services/deck/internal/scoring/processor_validate.go
@@ -8,7 +8,32 @@ func (processor *Processor) validate() (ResultType, error) {
 	// TODO: Check for duplicate scores
 	// TODO: For passed scores, validate the replay
 	// TODO: Run touchscreen detection
-	// TODO: Calculate ppv2 / ppv2
 	// TODO: Check pp limit for user
+
+	// TODO: Add a run() wrapper similar to finalize()
+	processor.calculatePPv2()
+	processor.calculatePPv1()
 	return ResultAccepted, nil
+}
+
+func (processor *Processor) calculatePPv2() {
+	if !processor.context.State.PPv2.Available() {
+		return
+	}
+
+	pp, err := processor.context.State.PPv2.CalculatePerformance(processor.submission.Score)
+	if err != nil {
+		processor.AddWarning("calculate ppv2: %w", err)
+		return
+	}
+	processor.submission.PP = pp
+}
+
+func (processor *Processor) calculatePPv1() {
+	pp, err := processor.context.State.PPv1.CalculatePerformance(processor.submission.Score)
+	if err != nil {
+		processor.AddWarning("calculate ppv1: %w", err)
+		return
+	}
+	processor.submission.PPv1 = pp
 }
