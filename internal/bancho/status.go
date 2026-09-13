107319caa39d147a87263cf1e8cf007925432b3a
Lekuru <contact@lekuru.xyz>
2026-09-13 11:35:22 +0200

    Add `version_string` to status store

--- internal/bancho/status.go
+++ internal/bancho/status.go
@@ -19,6 +19,7 @@ type Status struct {
 	BeatmapId       int                    `redis:"beatmap_id"`
 	ClientHash      string                 `redis:"hash"`
 	ClientVersion   int                    `redis:"version"`
+	ClientString    string                 `redis:"version_string"`
 }
 
 var requiredStatusFields = [...]string{
@@ -30,6 +31,7 @@ var requiredStatusFields = [...]string{
 	"beatmap_id",
 	"hash",
 	"version",
+	"version_string",
 }
 
 type StatusStore struct {
